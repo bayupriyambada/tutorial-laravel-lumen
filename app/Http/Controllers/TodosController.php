8db075d6ea384e9ea645a9952d57b9256620c864
bayupb <kampuspb.bayu@gmail.com>
2022-08-29 22:05:31 +0700

    add todos

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -16,4 +16,39 @@ class TodosController extends Controller
             'data' => $getAlltodos
         ]);
     }
+
+    public function store(Request $request)
+    {
+        $postsTodos = new Todos();
+        $postsTodos->name_todos = $request->name_todos;
+        $postsTodos->description_todos = $request->description_todos;
+        $postsTodos->save();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Successfully post data todos',
+            'data' => $postsTodos
+        ]);
+    }
+    public function update(Request $request, $id)
+    {
+        $updatedTodos = Todos::find($id);
+        $updatedTodos->name_todos = $request->name_todos;
+        $updatedTodos->description_todos = $request->description_todos;
+        $updatedTodos->save();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Successfully Update data todos',
+            'data' => $updatedTodos
+        ]);
+    }
+    public function destroy($id)
+    {
+        $deleteTodos = Todos::find($id);
+        $deleteTodos->delete();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Successfully delete data todos',
+            'data' => $deleteTodos
+        ]);
+    }
 }
